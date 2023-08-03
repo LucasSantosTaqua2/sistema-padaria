43d938faafe73a6c546e77e959a8473c41d30a06
21013 <21013@LAB1MAQ02>
2023-08-03 09:55:42 -0300

    Add project files.

---
+++ sistema-padaria/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace sistema_padaria.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
